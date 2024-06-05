a4042af1848cd9bb107f1411ba266b57954519d9
montessa-young <montessakayyoung@gmail.com>
2024-06-05 13:12:38 -0600

    Add Unit 2 material

---
+++ Week 2 - Variable Assignment/2.1PredictRunCode.py
@@ -0,0 +1,20 @@
+# Predict and Run Example 1
+# Add comments to the code below to explain what it does
+
+firstName = "Andy"
+
+print(firstName)
+
+# Predict and Run Example 2
+# Add comments to the code below to explain what it does
+
+lastName = "Colley"
+
+fullName = firstName + " " + lastName
+
+print(fullName)
+
+# Predict and Run Example 3
+# Add comments to the code below to explain what it does
+
+print("Hello " + firstName + ". Your full name is " + fullName + ".")
---
+++ Week 2 - Variable Assignment/2.3 ModifyCode.py
@@ -0,0 +1,12 @@
+# Variable Assignment - Modify
+
+# You can combine variables with strings in an output
+
+name1 = "Axl"
+name2 = "Slash"
+# Add 2 more variables to store 'Duff' and 'Izzy'
+
+# This line uses concatenation to join the variables together with the string 'and' to make a sentence.
+
+# Complete the line to output your added variables
+print(name1 + " and " + name2 + " and ")
