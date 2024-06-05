d2681c543242fc24214c49fc8cc27b21cc770e7f
montessa-young <montessakayyoung@gmail.com>
2024-06-05 15:52:42 -0600

    Add Unit 4 material

---
+++ Week 4 - Calculations/4.1PredictRunCode.py
@@ -0,0 +1,28 @@
+# Add comments to this code to predict what it will do.
+print(8 + 2)
+
+print( 8 - 2)
+
+print(8 * 2)
+
+print(10 / 4)
+
+print(10 // 4)
+
+# This code uses variables in place of numbers in the calculation.  Add comments to explain how it works.
+
+num1 = 20
+num2 = 5
+
+result = num1 * num2
+
+print(result)
+
+# This code uses input to assign data into a variable.  Add comments to explain how it works.
+print("Enter a number")
+num1 = int(input())
+num2 = 2
+
+result = num1 // num2
+
+print(result)
