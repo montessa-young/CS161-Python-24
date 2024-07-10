737a6a52d474d7d429acc9c547723aa03c6011c2
montessa-young <montessakayyoung@gmail.com>
2024-07-10 09:59:10 -0600

    Add Unit 11 material.

---
+++ Week 11 - Built-In Functions/11.1CodeComment.py
@@ -0,0 +1,35 @@
+check_list = [123.45, 268.95, -123.487, 1000, 1200.56, -987.00, 1500.145, -543, -222, 800]
+#
+print("The length of the list is this many items:")
+print(len(check_list))
+#
+x = max(check_list)
+print(x)
+#
+y = min(check_list)
+print(y)
+#
+k = abs(-123.4)
+print(k)
+#
+j = pow(10,3)
+print(j)
+#
+m = round(check_list[2],2)
+print(m)
+#
+n = sum(check_list)
+print(n)
+#
+print(dir(1))
+#
+import time
+print("Do you sometimes need a break?")
+time.sleep(2)
+print("I thought so")
+#
+import random
+print("I'm thinking of a number")
+q = random. randint(0, 1000)
+print(q)
+
---
+++ Week 11 - Built-In Functions/11.2FixErrorKey.py
@@ -0,0 +1,20 @@
+my_list = [1, 3, 5, 7, 9, 11, 13]
+my_other_list = [2, 4, 6, 8, 10]
+
+print(my_list)
+print(my_other_list)
+
+print(sum(my_list))
+
+y = sum(my_other_list)
+print("This is the sum of my other list: " + str(y))
+
+q = len(my_list)
+print(q)
+
+m = max(my_other_list)
+print("This is the highest number in my other list: " + str(m))
+
+n = min(my_list)
+print("This is the smallest number in my list: ")
+print(min(my_list))
